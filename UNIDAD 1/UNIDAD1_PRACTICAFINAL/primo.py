3e581d17f2c270933842329469ef350deed9ffa7
Diego Guijarro <diego.gsal08@gmail.com>
2026-08-27 10:43:59 -0600

    update primo.py

--- UNIDAD 1/UNIDAD1_PRACTICAFINAL/primo.py
+++ UNIDAD 1/UNIDAD1_PRACTICAFINAL/primo.py
@@ -1,9 +1,9 @@
-n = int(input("Introduce un numero: "))
-i=2
+n=input("Ingrese un número: ")
+n=int(n)
+if n<=1:
+    print("no es primo")
+    i=2
 while i<n:
     if n%i==0:
-        print("No es primo")
-        break
-    i+=1
-else:
-    print("Es primo")
+        print("es primo")
+    elif n%i==0:  i==n
