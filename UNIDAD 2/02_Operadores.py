e683047b75b6ba33e61b56199dd1a9501fbdab69
Diego Guijarro <diego.gsal08@gmail.com>
2026-09-01 11:51:48 -0600

    create UNIDAD 2

---
+++ UNIDAD 2/02_Operadores.py
@@ -0,0 +1,6 @@
+Operador = input("Ingrese operacion(+,-,*,/): ")
+O1 = (input("Ingrese primer numero: "))
+O2 = (input("Ingrese segundo numero: "))
+res = O1 + Operador + O2
+res = eval(res)
+print("El resultado es: ", res)
